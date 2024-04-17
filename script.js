2cb147186d26350135f339b3bed5da14b57ebfd4
UngDavid730 <124002445+UngDavid730@users.noreply.github.com>
2024-04-16 21:03:57 -0700

    init commit

---
+++ script.js
@@ -0,0 +1,43 @@
+function main(){
+
+}
+
+let vb = [
+    ['What is the Bit Stream for section a?','100',fig_venn3,'abc'],
+    ['What is the Bit Stream for section b?','010',fig_venn3,'abc'],
+    ['What is the Bit Stream for section c?','001',fig_venn3,'abc'],
+    ['What is the Bit Stream for section d?','110',fig_venn3,'abc'],
+    ['What is the Bit Stream for section e?','101',fig_venn3,'abc'],
+    ['What is the Bit Stream for section f?','011',fig_venn3,'abc'],
+    ['What is the Bit Stream for section g?','111',fig_venn3,'abc'],
+    ['What is the Bit Stream for section h?','000',fig_venn3,'abc']
+]
+
+let vd = [
+    ['What is the Disjunctive Normal Form of section a?','An_Bn_C',fig_venn3,'abc'],
+    ['What is the Disjunctive Normal Form of section b?','_AnBn_C',fig_venn3,'abc'],
+    ['What is the Disjunctive Normal Form of section c?','_An_BnC',fig_venn3,'abc'],
+    ['What is the Disjunctive Normal Form of section d?','AnBn_C',fig_venn3,'abc'],
+    ['What is the Disjunctive Normal Form of section e?','An_BnC',fig_venn3,'abc'],
+    ['What is the Disjunctive Normal Form of section f?','_AnBnC',fig_venn3,'abc'],
+    ['What is the Disjunctive Normal Form of section g?','AnBnC',fig_venn3,'abc'],
+    ['What is the Disjunctive Normal Form of section h?','_An_Bn_C',fig_venn3,'abc']
+]
+
+let bd = [
+    ['What is the Disjunctive Normal Form of 100?','An_Bn_C',fig_venn3,'bit'],
+    ['What is the Disjunctive Normal Form of 010?','_AnBn_C',fig_venn3,'bit'],
+    ['What is the Disjunctive Normal Form of 001?','_An_BnC',fig_venn3,'bit'],
+    ['What is the Disjunctive Normal Form of 110?','AnBn_C',fig_venn3,'bit'],
+    ['What is the Disjunctive Normal Form of 101?','An_BnC',fig_venn3,'bit'],
+    ['What is the Disjunctive Normal Form of 011?','_AnBnC',fig_venn3,'bit'],
+    ['What is the Disjunctive Normal Form of 111?','AnBnC',fig_venn3,'bit'],
+    ['What is the Disjunctive Normal Form of 000?','_An_Bn_C',fig_venn3,'dnf']
+]
+
+
+
+
+
+main()
+
